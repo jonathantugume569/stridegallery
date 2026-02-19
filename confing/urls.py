50598e8bfd89a5af10b9b6b1f2616d3e89fd12f2
jonathantugume569 <jonathantugume569@gmail.com>
2026-02-19 17:14:08 +0300

    removed if

--- confing/urls.py
+++ confing/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
 ]
 
 # ===== MEDIA FILES (for development) =====
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # ===== REACT CATCH-ALL (put last!) =====
 urlpatterns += [
