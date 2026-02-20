0981c17ecd7b849d21c270e077333ff6457bf62e
jonathantugume569 <jonathantugume569@gmail.com>
2026-02-20 16:33:50 +0300

    changed model

--- store/models.py
+++ store/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField   # ✅ ADD THIS
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='categories/')
+    image = CloudinaryField('image', folder='categories/')   # ✅ CHANGE
 
     def __str__(self):
         return self.name
@@ -11,10 +12,8 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='products/')
+    image = CloudinaryField('image', folder='products/')   # ✅ CHANGE
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
-    
-
